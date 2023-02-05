fa433803546521940e1706bf57a256526a6b1e3d
MeToShin1 <120088122+MeToShin1@users.noreply.github.com>
2023-02-05 20:49:45 +0700

    Update CONFIG.js

--- CONFIG.js
+++ CONFIG.js
@@ -1,10 +1,10 @@
 const CONFIGDATA = {
-  titleHeader: "Làm người yêu tớ nhé I Love You",
+  titleHeader: "Làm người yêu em nhé I Love You",
   descriptionHeader:
-    "Nếu cậu thoát ra và không trả lời thì có nghĩa là cậu đã ĐỒNG Ý rồi đấy ^^",
+    "Nếu chị thoát ra và không trả lời thì có nghĩa là chị đã ĐỒNG Ý rồi đấy ^^",
   buttonYes: "Đồng Ý",
   buttonNo: "Không! Còn lâu nhé.",
-  titleModar: "Cảm ơn em vì đồng ý",
+  titleModar: "Cảm ơn chị vì đồng ý",
   descriptionModar:
-    'Tớ biết là cậu sẽ đồng ý làm người yêu tớ mà ^^ </br> Cám ơn cậu đã đồng ý để cho tớ được che chở và bảo vệ cậu.Đối với tớ, cậu mãi là cô bé :)) </br> "Thích cậu nhiều đến mấy, cậu sẽ hiểu thôi. </br>  Tớ thích cậu nhiều đến mấy, cậu sẽ hiểu thôi "',
+    'Em biết là chị sẽ đồng ý làm người yêu em mà ^^ </br> Cám ơn chị Nguyễn Kim Trinh đã đồng ý :)) </br> "Em code tặng chị đấy. </br>  Nguyễn Kim Trinh code by Nguyễn Nhật Duy "',
 };
